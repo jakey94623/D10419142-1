5f5c90ba66ef7fd5b2daf6972360a9aa00defda2
jakey94623 <32121408+jakey94623@users.noreply.github.com>
2018-02-28 16:14:47 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -26,7 +26,7 @@ require_once('./LINEBotTiny.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 
-$link=mysqli_connect("127.0.0.1","id901974_linebot","123456","id901974_linebot","3306");
+$link=mysqli_connect("localhost","id901974_linebot","123456","id901974_linebot","3306");
 mysqli_query($link,"select * from test",MYSQLI_USE_RESULT);
                 mysqli_close($link);
 
