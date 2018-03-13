e8d1a6ad9d5587da861411ebd4f3f417f0ef3e09
jakey94623 <32121408+jakey94623@users.noreply.github.com>
2018-03-13 18:29:01 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -46,7 +46,7 @@ foreach ($client->parseEvents() as $event) {
 			   
 			   
 			@$mysqli = new mysqli('e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "lptrv8w6oc62hrpr", "iagiyml96j33de6q", "ifz67f5o6szf2gdu","3306");
-			    $cool=mysqli_query('e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "lptrv8w6oc62hrpr", "iagiyml96j33de6q", "ifz67f5o6szf2gdu","3306","select Q from test");
+			    $cool=mysqli_query($mysqli,select Q from test);
 			    if(mysqli_connect_errno()){ $debugmsg='資料庫連線失敗'; //資料庫連線失敗
 				}else{
 					$mysqli->close();
