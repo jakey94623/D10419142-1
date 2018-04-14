3d76c12949611a914c13dfc840401ad3aca955d4
jakey94623 <32121408+jakey94623@users.noreply.github.com>
2018-04-14 22:27:53 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Copyright 2016 LINE Corporation
  *
@@ -15,18 +14,10 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 require_once('./LINEBotTiny.php');
 require_once __DIR__ . '/../src/LINEBot/Response.php';
-
-
-
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-
-
-
-
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
@@ -45,19 +36,68 @@ foreach ($client->parseEvents() as $event) {
 			    $debugmsg='123456';
 			   
 			   
-			$mysqli = new mysqli('e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "lptrv8w6oc62hrpr", "iagiyml96j33de6q", "ifz67f5o6szf2gdu","3306");
+			
+			    $mysqli = new mysqli('e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', "lptrv8w6oc62hrpr", "iagiyml96j33de6q", "ifz67f5o6szf2gdu","3306"); 
 			    
-			    $cool=mysqli_query($mysqli,"select Q from c304msgdata");
-			    mysqli_query($mysqli,"INSERT INTO c304msgdata (Q, A)VALUES ('555','666')");
 			    if(mysqli_connect_errno()){ $debugmsg='資料庫連線失敗'; //資料庫連線失敗
 				}else{
-					$mysqli->close();
-				}
+				        mysqli_query($mysqli,"SET NAMES 'utf8'");
+					
+			    }
+	    
 			    
-			
+			    
+			    
+                       		 $mysqli->query("Insert INTO test (msg) values ('$m_message')");//成功會回傳 object 失敗則回傳 null
+				
+                   if(preg_match("/進/","$m_message")){
+		   $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                //'text' => $m_message ."\n" . $roomid."\n". date('Y-m-d h:i:sa') . "\n" . $id . "\n" . $groupid
+				 'text' => $debugmsg
+				 
+		   }else {
+		    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                //'text' => $m_message ."\n" . $roomid."\n". date('Y-m-d h:i:sa') . "\n" . $id . "\n" . $groupid
+				 'text' => 'error'
+		   }
+			   
+			   
+			 
 			    
                 	if($m_message=="安安")
                 	{
+				$result = $mysqli->query("SELECT Q FROM test");//成功會回傳 object 失敗則回傳 null
+				if($result==null){ /* 資料庫語法錯誤 */
+					//$this->console_log .='資料庫語法錯誤';
+					//$this->console_log .='語法錯誤>'.(string)$mysqli->errno.(string)$mysqli->error.'<';
+				}else{
+					//$this->console_log .='更改筆數>'.(int)$mysqli->affected_rows.'<';
+					//$this->console_log .='插入流水號>'.(int)$mysqli->insert_id.'<';
+					//$this->console_log .='欄位數量>'.(int)$result->field_count.'<';
+					//$this->console_log .='資料筆數>'.(int)$result->num_rows.'<';
+					
+					if((int)$result->num_rows==0){//沒有值會錯誤
+						
+					}else{
+						
+						
+						while ($row = $result->fetch_assoc() ){
+							$cool=$row['Q'];
+						}	
+							
+						
+						$result->close();
+					}
+					
+				}
 				
                 		$client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
@@ -130,7 +170,21 @@ foreach ($client->parseEvents() as $event) {
             )
         )
                     	));	
-			}
+			}else if($m_message=="hello"){
+				
+				
+				$client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                //'text' => $m_message ."\n" . $roomid."\n". date('Y-m-d h:i:sa') . "\n" . $id . "\n" . $groupid
+				 'text' => $debugmsg
+				 
+                            )	
+                        )
+                    	));			
+                	}
                     break;
                     
                     
