9517d731aaba1a7b1e34dc8f6c9cf28fee2e63bd
jakey94623 <32121408+jakey94623@users.noreply.github.com>
2018-07-03 14:39:43 +0800

    Update index01.php

--- web/index01.php
+++ web/index01.php
@@ -111,7 +111,7 @@ foreach ($client->parseEvents() as $event) {
 						
 						if($msg2 == "進"){
 							$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("驗證逾時");
-							$response = $bot->pushMessage($groupid, $textMessageBuilder);
+							$response = $bot->pushMessage($userId, $textMessageBuilder);
 							$sql="delete from code where numbercode='$key' and userid='$userId'";
 							$result = $mysqli->query($sql);
 							$sql="UPDATE 304ex SET worktype='逾時' where worktype='' and vcode='' and userid='$userId';";
